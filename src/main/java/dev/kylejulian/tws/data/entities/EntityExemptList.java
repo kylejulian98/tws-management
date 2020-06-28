e041253a2da1b82dd123dbc7f481119e37339a40
Kyle Julian <38759683+kylejulian98@users.noreply.github.com>
2020-06-28 17:21:57 +0100

    Add missing files that were involved in the refactor of the previous commit. Build was failing on the build server.

---
+++ src/main/java/dev/kylejulian/tws/data/entities/EntityExemptList.java
@@ -0,0 +1,43 @@
+package dev.kylejulian.tws.data.entities;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class EntityExemptList {
+
+    private ArrayList<UUID> playerIds;
+
+    private int maxPageCount;
+
+    /**
+     * Get Player Ids retrieved from the Database
+     * @return Array of Player UUIDs
+     */
+    public ArrayList<UUID> getPlayerIds() {
+        return this.playerIds;
+    }
+
+    /**
+     * Get the maximum number of pages
+     * @return int max number of possible pages (with specified page index and page size)
+     */
+    public int getMaxPageCount() {
+        return this.maxPageCount;
+    }
+
+    /**
+     * Set the Player Ids
+     * @param playerIds Player Ids to set
+     */
+    public void setPlayerIds(ArrayList<UUID> playerIds) {
+        this.playerIds = playerIds;
+    }
+
+    /**
+     * Set the maximum number of pages
+     * @param maxPageCount maximum number of pages
+     */
+    public void setMaxPageCount(int maxPageCount) {
+        this.maxPageCount = maxPageCount;
+    }
+}
